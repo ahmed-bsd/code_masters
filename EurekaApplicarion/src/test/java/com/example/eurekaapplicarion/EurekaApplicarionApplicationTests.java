41420de21f58eea4902e8a4577ae8df8fc10f827
nourGu <nourelhouda.guelmami@esprit.tn>
2023-10-15 23:06:38 +0100

    first commit

---
+++ EurekaApplicarion/src/test/java/com/example/eurekaapplicarion/EurekaApplicarionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaapplicarion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicarionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
